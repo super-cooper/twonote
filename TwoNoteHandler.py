d4eb7b3658289687f332d28663aa62f878196d14
Thomas Nemeh <tnemeh@oberlin.edu>
2018-12-03 13:59:46 -0500

    additional features

--- TwoNoteHandler.py
+++ TwoNoteHandler.py
@@ -1,9 +1,9 @@
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk, Pango
+from gi.repository import Gtk, Gdk, Pango
 
 builder = Gtk.Builder()
-builder.add_from_file("/home/thomas/Documents/TestNotepad2.glade")
+builder.add_from_file("/home/thomas/Notepad.glade")
 textview = builder.get_object("textview")
 buffer = textview.get_buffer()
 window = builder.get_object("MainWindow")
@@ -36,13 +36,48 @@ spacing_blocked = True
 
 tag_found = buffer.create_tag("found", background="yellow")
 
+tag_strike = buffer.create_tag("strike", strikethrough=True)
+
+textview.set_wrap_mode(Gtk.WrapMode.WORD_CHAR)
+wrapping_blocked = False
+
+color_chooser = builder.get_object("color_chooser")
+tag_color = buffer.create_tag("Gdk.RGBA(red=0.000000, green=0.000000, blue=0.000000, alpha=1.000000)", foreground_rgba=Gdk.RGBA(red=0.000000, green=0.000000, blue=0.000000, alpha=1.000000))
+
+highlight_chooser = builder.get_object("highlight_chooser")
+tag_highlight = buffer.create_tag("highlight: Gdk.RGBA(red=0.000000, green=0.000000, blue=0.000000, alpha=0.000000)", background_rgba=Gdk.RGBA(red=0.000000, green=0.000000, blue=0.000000, alpha=0.000000))
 
 active_tags = []
 
 buffer.create_mark("old_pos", buffer.get_iter_at_mark(buffer.get_insert()), True)
 
+
 #implement emoji chooser
 #strike through property?
+
+#TODO: treeview
+#create the model
+store = Gtk.TreeStore(str)
+#returns TreeIter poiting to this row
+new_text = store.append(None, ["newfile.txt"])
+
+#the treeview shows the model
+treeview = builder.get_object("treeview")
+treeview.set_model(store)
+single_click = True
+treeview.set_activate_on_single_click(single_click)
+
+# the cellrenderer for the first column
+renderer = Gtk.CellRendererText()
+# text attribute gets value from column 2
+column_new_text = Gtk.TreeViewColumn("Notes", renderer, text=0)
+resizable = True
+column_new_text.set_resizable(resizable)
+treeview.append_column(column_new_text)
+''' use destroy to remove widget from boc
+textview_box.remove(textview)
+textview.destroy()'''
+#print(treeview.get_activate_on_single_click())
 
 class SearchDialog(Gtk.Dialog):
 
@@ -54,11 +89,17 @@ class SearchDialog(Gtk.Dialog):
 
         box = self.get_content_area()
 
-        label = Gtk.Label("Insert text you want to search for:")
-        box.add(label)
+        label_find = Gtk.Label("Insert text you want to search for:")
+        box.add(label_find)
 
-        self.entry = Gtk.Entry()
-        box.add(self.entry)
+        self.entry_find = Gtk.Entry()
+        box.add(self.entry_find)
+
+        label_replace = Gtk.Label("Insert replacement text (optional):")
+        box.add(label_replace)
+
+        self.entry_replace = Gtk.Entry()
+        box.add(self.entry_replace)
 
         self.show_all()
 
@@ -120,37 +161,39 @@ class Handler:
     def edit_input(buffer):
         global tag_font
         global tag_size
+        global tag_color
+        global tag_highlight
         #for x in active_tags:
             #print(x)
 
         buffer.remove_tag_by_name("found", buffer.get_start_iter(), buffer.get_end_iter())
         buffer.apply_tag(tag_font, buffer.get_iter_at_mark(buffer.get_mark("old_pos")), buffer.get_iter_at_mark(buffer.get_insert()))
         buffer.apply_tag(tag_size, buffer.get_iter_at_mark(buffer.get_mark("old_pos")), buffer.get_iter_at_mark(buffer.get_insert()))
+        buffer.apply_tag(tag_color, buffer.get_iter_at_mark(buffer.get_mark("old_pos")), buffer.get_iter_at_mark(buffer.get_insert()))
+        buffer.apply_tag(tag_highlight, buffer.get_iter_at_mark(buffer.get_mark("old_pos")), buffer.get_iter_at_mark(buffer.get_insert()))
 
         for x in active_tags:
             buffer.apply_tag_by_name(x, buffer.get_iter_at_mark(buffer.get_mark("old_pos")), buffer.get_iter_at_mark(buffer.get_insert()))
 
     #should we change the way this works?
+    #fixed after user testing
     def change_justification(self, button):
         global justification_blocked
         global main_justification_name
 
-        if (justification_blocked == True):
-            justification_blocked = False
-            return
-
         #do not execute function when button is deactivated
         if(button.get_active() == False):
             return
+
 
         name = Gtk.Buildable.get_name(button)
         bounds = buffer.get_selection_bounds()
         if len(bounds) != 0:
             start, end = bounds
 
-        if len(bounds) != 0 and (not start.is_start() or not end.is_end()):
-            #print(name)
-            print(end.is_end())
+        if len(bounds) != 0 and (not start.is_start() or not end.is_end()) and justification_blocked == False:
+            #print("if: " + name)
+            #print(end.is_end())
             buffer.apply_tag_by_name(name, start, end)
 
             #restore main justification
@@ -158,27 +201,33 @@ class Handler:
             main_justification = builder.get_object(main_justification_name)
             main_justification.set_active(True)
         else:
-            #print(name)
+            #print("else: "+ name)
             main_justification_name = name
             textview.set_justification(justifications[name])
+
+        if (justification_blocked == True):
+                justification_blocked = False
 
         insert = buffer.get_iter_at_mark(buffer.get_insert())
         buffer.place_cursor(insert)
 
-
+    #TODO: if you highlight backwards, you get wierd behaviour
+    #For when we align highlighted text with the main justification
     def change_justification_helper(self, button):
         bounds = buffer.get_selection_bounds()
         name = Gtk.Buildable.get_name(button)
         #print("outside")
 
         if (len(bounds) != 0 and button.get_active() == True):
-            #print(name)
+            print(name)
+            print("inside")
             start, end = bounds
             buffer.remove_tag_by_name("left", start, end)
             buffer.remove_tag_by_name("center", start, end)
             buffer.remove_tag_by_name("right", start, end)
             buffer.remove_tag_by_name("fill", start, end)
-            buffer.apply_tag_by_name(name, start, end)
+            #commenting this line out aligns highlighted text once again with the group
+            #buffer.apply_tag_by_name(name, start, end)
             insert = buffer.get_iter_at_mark(buffer.get_insert())
             buffer.place_cursor(insert)
 
@@ -220,21 +269,101 @@ class Handler:
             if start.get_offset() == buffer.get_char_count():
                 start = buffer.get_start_iter()
 
-            Handler.search_and_mark(self, dialog.entry.get_text(), start) #TODO: missing argument: start
+            Handler.search_and_mark(self, dialog.entry_find.get_text(), dialog.entry_replace.get_text(), start) #TODO: missing argument: start
 
         dialog.destroy()
 
     #TODO: change this
-    def search_and_mark(self, text, start):
+    #TODO: Make user apply tags to highlighted text
+    def search_and_mark(self, text_entry, text_replace, start):
         global buffer
         global tag_found
         end = buffer.get_end_iter()
-        match = start.forward_search(text, 0, end)
+        match = start.forward_search(text_entry, 0, end)
 
         if match is not None:
             match_start, match_end = match
-            buffer.apply_tag(tag_found, match_start, match_end)
-            Handler.search_and_mark(self, text, match_end) #TODO: What is this 'self'. Deactivate tag when you start typing again
+            #mark = buffer.create_mark("mark", match_start, False)
+            #mark2 = buffer.create_mark("mark2", match_end, True)
+            if (len(text_replace) > 0):
+                #user_interaction = True
+                #buffer_editable = True
+                #buffer.select_range(match_start, match_end)
+                #buffer.delete_selection(user_interaction, buffer_editable)
+                buffer.delete(match_start, match_end)
+                buffer.insert_with_tags(match_end, text_replace, tag_found)
+            else:
+                buffer.apply_tag(tag_found, match_start, match_end)
+            Handler.search_and_mark(self, text_entry, text_replace, match_end) #TODO: What is this 'self'. Deactivate tag when you start typing again. fixed after user testing
+
+    def test(text, a ,b ,c, d):
+        print("ahh!")
+
+    def test2(self, button, etc):
+        global window
+        print("hi")
+
+    def set_wrapping(self, button):
+        global textview
+        global wrapping_blocked
+
+        if (wrapping_blocked == False):
+            name = Gtk.Buildable.get_name(button)
+            if (name == "word_wrapping"):
+                textview.set_wrap_mode(Gtk.WrapMode.WORD)
+            elif (name == "char-wrapping"):
+                textview.set_wrap_mode(Gtk.WrapMode.CHAR)
+            elif (name == "both"):
+                textview.set_wrap_mode(Gtk.WrapMode.WORD_CHAR)
+            else:
+                textview.set_wrap_mode(Gtk.WrapMode.NONE)
+        else:
+            wrapping_blocked = True
+
+    def change_color(self, widget, number):
+        global buffer
+        global tag_table
+        global tag_color
+        RGBA = widget.get_rgba()
+
+        if (tag_table.lookup(str(RGBA)) == None):
+            tag_color = buffer.create_tag(str(RGBA), foreground_rgba=RGBA)
+        else :
+            tag_color = tag_table.lookup(str(RGBA))
+
+        #TEST THIS: remove tag?
+        bounds = buffer.get_selection_bounds()
+        if len(bounds) != 0:
+            start, end = bounds
+            buffer.apply_tag(tag_color, start, end)
+
+    def change_highlight(self, widget, number):
+        global buffer
+        global tag_table
+        global tag_highlight
+        RGBA = widget.get_rgba()
+
+        if (tag_table.lookup("highlight: " + str(RGBA)) == None):
+            tag_highlight = buffer.create_tag("highlight: " + str(RGBA), background_rgba=RGBA)
+        else :
+            tag_highlight = tag_table.lookup("highlight: " + str(RGBA))
+
+        #TEST THIS: remove tag?
+        bounds = buffer.get_selection_bounds()
+        if len(bounds) != 0:
+            start, end = bounds
+            buffer.apply_tag(tag_highlight, start, end)
+
+
+    def open_color_dialog(self, button):
+        Gtk.Widget.show_all(builder.get_object("color_chooser"))
+
+    def open_highlight_dialog(self, button):
+        Gtk.Widget.show_all(builder.get_object("highlight_chooser"))
+
+    def close_dialog(self, widget, number):
+        print(widget.get_rgba())
+        Gtk.Widget.hide(widget)
 
     #why the pointer?
     #or else python interpreter will continue running
@@ -245,9 +374,13 @@ class Handler:
 builder.connect_signals(Handler())
 buffer.connect("insert-text", Handler.get_old_pos)
 buffer.connect("end-user-action", Handler.edit_input)
+#TODO: not working
+textview.connect("move-cursor", Handler.test)
+#color_chooser.connect("response", Handler.close_dialog)
 #buffer.connect("group-changed", Handler.set_spacing)
 
 window = builder.get_object("MainWindow")
+textview_box = builder.get_object("textview_box")
 window.show_all()
 
 Gtk.main()
